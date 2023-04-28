7156fbaf03b202bc505a1923d7d7083bc4862171
Dashencko Evgeny <j.dashencko@gmail.com>
2023-04-28 16:04:16 +0300

    добавлен myscript

---
+++ myscript.js
@@ -0,0 +1,7 @@
+var footer_logo = document.getElementById("footer_logo")
+var notion = document.getElementById("notion")
+
+alert(notion.style.offsetWidth)
+
+footer_logo.style.offsetWidth = notion.style.offsetWidth
+
